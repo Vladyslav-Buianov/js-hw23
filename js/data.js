29d4b43f193105dcebfe14f67f01ac55f06d50a7
Vladyslav <vitbuianov.20130225@gmail.com>
2026-06-30 15:18:49 +0200

    fixes

--- js/data.js
+++ js/data.js
@@ -1,24 +1,24 @@
-const imagesRef = document.querySelectorAll(".images__img");
-// дістаємо доступ до фотків
+const itemsRef = document.querySelectorAll(".images__item");
+// дістаємо доступ до лішків
 
-if (imagesRef.src = "") {
-    imagesRef.style.height = "200px";
-    imagesRef.style.backgroundColor = "#f0f0f0";
-}// Якщо фотка ще не заределася то у ней такі стилі
-
-const imagesObserver = new IntersectionObserver((entries, observer)=>{
+const lisObserver = new IntersectionObserver((entries, observer)=>{
     entries.forEach((entry)=>{
         if(entry.isIntersecting){
-            entry.target.src = entry.target.dataset.src;
-            entry.target.classList.add("active");
+            const img = entry.target.querySelector(".images__img");
+            // дістаємо доступ до фотків
+            if (img) {
+                img.src = img.dataset.src;
+                img.classList.add("active");
+            }
             observer.unobserve(entry.target);
-        }// якщо фотка в полі зору то втавляємо src з дата-атрибута, додаєм анімації і перестаємо дивитися за нею.
+        }
+        // якщо лішка в полі зору то втавляємо src з дата-атрибута фотки до фотки, додаєм анімації і перестаємо дивитися за лішкою.
     })
 }, {rootMargin: "40px" 
-// тут написана довжина від фотки у якій вона буде в полі зору
+// тут написана довжина від лішки у якій вона буде в полі зору
 })
 
-imagesRef.forEach(images => {
-    imagesObserver.observe(images)
+itemsRef.forEach(lis => {
+    lisObserver.observe(lis);
 })
-// дивимось за кожною фоткою
+// дивимось за кожною лішкою
